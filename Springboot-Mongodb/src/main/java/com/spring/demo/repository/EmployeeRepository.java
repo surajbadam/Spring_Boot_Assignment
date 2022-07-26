7095005fe76438977e5f2b25b4a61874cc0b73b7
surajbadam <72784198+surajbadam@users.noreply.github.com>
2022-07-26 14:11:58 +0530

    Delete Springboot-Mongodb directory

--- Springboot-Mongodb/src/main/java/com/spring/demo/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.spring.demo.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.spring.demo.model.Employee;
-
-public interface EmployeeRepository extends MongoRepository<Employee, Integer> {
-
-}
